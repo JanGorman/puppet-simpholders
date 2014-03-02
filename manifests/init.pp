7db77f65157334675cefbe29b17bf26274cd84d5
Jan Gorman <gorman.jan@gmail.com>
2014-03-02 14:49:18 +0100

    lint fixes

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,11 @@
 # Public: Install SimPholders
-# 
+#
 # Examples
 #
 #   include simpholders
 class simpholders {
   package { 'SimPholders':
     provider => 'appdmg',
-    source => 'http://simpholders.com/site/assets/files/1007/simpholders-1_5_0.dmg'
+    source   => 'http://simpholders.com/site/assets/files/1007/simpholders-1_5_0.dmg'
   }
 }
