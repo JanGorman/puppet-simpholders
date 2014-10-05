b30347a056d2fe53a224bebd42feddc4a369db71
Jan Gorman <gorman.jan@gmail.com>
2014-10-05 21:12:37 +0200

    Update to simpholders 2

--- spec/classes/simpholders_spec.rb
+++ spec/classes/simpholders_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe 'simpholders' do
   it do
     should contain_package('SimPholders').with({
-      :source => 'http://simpholders.com/site/assets/files/1007/simpholders-1_5_0.dmg',
-      :provider => 'appdmg'
+      :source => 'http://simpholders.com/site/assets/files/1098/sp20a-87.zip',
+      :provider => 'compressed_app'
     })
   end
 end
